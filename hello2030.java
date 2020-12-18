89d65ff29244a333ab99983bd4d2308bde8d1497
yangheejin123 <57510457+yangheejin123@users.noreply.github.com>
2020-12-18 23:48:59 +0900

    2주차 실습과제

---
+++ hello2030.java
@@ -0,0 +1,22 @@
+/*****************************
+
+* 프로그램명:자기소개 출력 프로그램
+
+* 작성자
+
+* 이름 :양희진
+
+* 작성일 :20200914
+
+*프로그램 설명 :학과, 학년, 입학년도, 이름을 출력하는 프로그램이다.
+
+*********************************/
+public class hello2030 {
+	public static void main(String[] args) {
+		System.out.println("소프트웨어학과");
+		System.out.println("2학년");
+		System.out.println("2019년 입학");
+		System.out.println("양희진");
+	}
+
+}
